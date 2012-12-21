7524af75fe64988fbbef0d1a25517b830edf63e1
Sean Steimer <sean.steimer@gmail.com>
2012-12-21 06:09:08 -0800

    modifications made for the index page, need to refactor other views still.

---
+++ head.php
@@ -0,0 +1,33 @@
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="<?php echo get_bloginfo('name'); ?>">
+    <title><?php
+    global $page, $paged;
+
+    // Add wp_title ()
+    wp_title( '|', true, 'right' );
+
+    // Add the blog name.
+    bloginfo( 'name' );
+
+    // Add the blog description for the home/front page
+    $site_description = get_bloginfo( 'description', 'display' );
+    if ( $site_description && ( is_home() || is_front_page() ) )
+        echo " | $site_description";
+
+    // Add a page number if necessary:
+    if ($paged >= 2 || $page >= 2)
+        echo ' | ' . sprintf(__('Page %s', 'shsteimer'), max($paged, $page));
+    ?></title>
+
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/styles.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/responsive-styles.css" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/prettify/prettify.css" />
+    
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?> " />
+
+    <?php wp_head(); ?>
+</head>
