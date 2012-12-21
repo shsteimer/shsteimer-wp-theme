5df38c4d234e9825e308870ad7ea52ee7bf070e2
Sean Steimer <sean.steimer@gmail.com>
2012-12-21 06:59:11 -0800

    changes to make search work in new template system

---
+++ content-search.php
@@ -0,0 +1,22 @@
+<div class="container-fluid">
+  <div class="row-fluid">
+    <div class="span7">
+    	<ul class="breadcrumb">
+    		<li>Found <?php echo $wp_query->post_count; ?> result(s) for '<?php the_search_query(); ?>'</li>
+    	</ul>
+      <?php 
+	if ( have_posts() ) :
+		while (have_posts()) {
+			the_post(); ?>
+			<?php get_template_part( 'post', 'search' ); ?>
+		<?php
+		}
+	else : ?>
+		<h1>Sorry, Nothing found.  Maybe you can find what you're looking for by searching?</h1><?php
+		get_search_form( );
+	endif; 
+	?>
+    </div>
+    <?php get_sidebar(); ?>
+  </div>
+</div> 
--- search.php
+++ search.php
@@ -7,50 +7,19 @@
  *
 */
 ?>
-<?php get_header(); ?>
-<div class="container-fluid">
-  <div class="row-fluid">
-    <div class="span7">
-    	<ul class="breadcrumb">
-    	<li>Found <?php echo $wp_query->post_count; ?> result(s) for '<?php the_search_query(); ?>'</li>
-    	</ul>
-      <?php 
-	if ( have_posts() ) {
-		while (have_posts()) {
-			the_post(); ?>
-			<div class="post well">
-				<a class="post-title" href="<?php the_permalink(); ?>" title="<?php echo esc_attr( the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">
-						<h2><?php the_title(); ?></h2>
-				</a>
-				<span class="post-info muted">Posted by <?php echo get_the_author(); ?> on <?php the_date('m.d.Y'); ?></span>
-				<div class="post-body"><?php the_content(); ?></div>
-				<p>Categories: <?php the_category(', '); ?></p> 
-				<?php if ( comments_open() ) : ?>
-					<div class="comments-link">
-						<?php comments_popup_link("0 comments","1 comment","% comments" ); ?>
-					</div><!-- .comments-link -->
-				<?php endif; // comments_open() ?>
-				<div class="post-social">
-					<ul>
-						<li>
-							<div class="fb-like" data-href="<?php the_permalink(); ?>" data-send="false" 
-								data-layout="button_count" data-width="450" data-show-faces="false" data-colorscheme="light"></div>
-						</li>
-						<li>
-							<a href="https://twitter.com/share" class="twitter-share-button" data-text="<?php the_title(); ?>" data-url="<?php the_permalink(); ?>" data-via="shsteimer">Tweet</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		<?php
-		}
-	} else { ?>
-		<h1>Sorry, Nothing found.  Maybe you can find what you're looking for by searching?</h1><?php
-		get_search_form( );
-	} 
-	?>
-    </div>
-    <?php get_sidebar(); ?>
-  </div>
-</div>  
-<?php get_footer(); ?>
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?> xmlns:fb="http://ogp.me/ns/fb#">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" <?php language_attributes(); ?> xmlns:fb="http://ogp.me/ns/fb#">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?> xmlns:fb="http://ogp.me/ns/fb#">
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?> xmlns:fb="http://ogp.me/ns/fb#">
+<!--<![endif]-->
+<?php get_template_part( 'head', 'search' ); ?>
+<?php get_template_part( 'body', 'search' ); ?>
+</html>
