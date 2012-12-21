44275b35adf9af0fa25bd26f7de8b5b0ee8217b1
Sean Steimer <sean.steimer@gmail.com>
2012-12-21 06:10:07 -0800

    changes to existing files for the index page refctor

--- footer.php
+++ footer.php
@@ -32,6 +32,4 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/prettify/prettify.js"></script>  
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>   
-</body>
-</html>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
